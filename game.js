c3ccbcc05d05cf272f8d2e6ffc02dd2a9ac0e555
zaiterassia <37081784+zaiterassia@users.noreply.github.com>
2018-03-21 22:20:28 +0100

    Update game.js

--- game.js
+++ game.js
@@ -39,15 +39,6 @@ for (var R=0; R<X; R++) {
 //   }
 // }
 
-
-/* var Row = 17, Col = 25;                                       // rows and columns
-var Player = false, IsOver = false ;
-var Start_Cell = (0,0) ;                                      // case départ pour un mouvement
-var Tree = {}, list = [] ;
-Fld = new Array(Row);                                         // fld matrice de 17*25 ele
-for (i=0; i<Row; i++)
-  Fld[i] = new Array(Col);
-*/
 init_matrice();
 create_board();
 
@@ -56,21 +47,21 @@ create_board();
 // initialise la matrice pour le plateau de jeu
 function init_matrice() {
   for (var R=0; R<4; R++) { // R pour row, C pour column
-    for (var C=12-R; C<12+R+1; C+=2) {
+    for (var C=12-R; C<=12+R; C+=2) {
       M[R][C] = 1;
-      M[16-R][C] = 2;
+      M[16-R][C] = 6;
     }
   }
   for (var R=4; R<8; R++) {
-    for (var C=12-R; C<12+R+1; C+=2) {
+    for (var C=12-R; C<=12+R; C+=2) {
       M[R][C] = -1;
       M[16-R][C] = -1;
     }
-    for (var C=R-4; C<10-R+1; C+=2) {
-      M[R][C] = 3;
+    for (var C=R-4; C<=10-R; C+=2) {
+      M[R][C] = 2;
       M[16-R][C] = 4;
-      M[R][24-C] = 5;
-      M[16-R][24-C] = 6;
+      M[R][24-C] = 3;
+      M[16-R][24-C] = 5;
     }
   }
   for (var C=4; C<21; C+=2) { M[8][C] = -1 }
@@ -124,45 +115,95 @@ function validate_movement(cell) {
   var C = cell.getAttribute('column');
   if (Start_Cell === (0,0)) { // premier click
     if (M[R][C] !== Player+1) return; // vérifie qu'on click sur le pion du joueur qui a la main // pourquoi Player+1 ?
-    Start_Cell = (R,C);
-    // RefreshScreen();
+    Start_Cell = (R,C);               // player = false =0 --> joueur de pion numéroté (1) = player +1; 
+    // RefreshScreen();               // player = true = 1  --> joueur (2) = player+1
     cell.firstChild.src = "images/pion" + M[R][C] + "vide.png";
-    get_traject(R,C);
-    M[R][C] = -1; // marquer la case depart vide pour ne pas l'utiliser comme pivot dans get_hope()
+    M[R][C] = -1;                    // marquer la case depart vide pour ne pas l'utiliser comme pivot dans get_hope()
   }
   else {
     if ((R,C) === Start_Cell) { // retour à la case départ annule le mouvement
       M[R][C] = Player+1;
       Start_Cell = (0,0);
-      cell.firstChild.src = "images/pion" + M[R][C] + ".png";
+      cell.firstChild.src = "images/pion" + M[R][C] + ".png";    //ok je vois l'intéret c'est mieux que d'utilser refershscreen
       // RefreshScreen();
     }
     if (M[R][C] !== -1) return ; // cell not empty
-        get_traject(Start_Cell[0], Start_Cell[1]);
-        if (!Tree[(R, C)]) {
-          alert("Invalid Move!");
-          return ;
-        }
-        traject = Tree[(R,C)];
-        make_move(traject);
-     }
+    get_traject(Start_Cell[0], Start_Cell[1]);
+    if (!Tree[(R, C)]) {
+      alert("Invalid Move!");
+      return ;
+    }
+    traject = Tree[(R,C)];
+    make_move(traject);
+  }
 }
 
 function get_traject(R, C) {
-  // // ces boucles se comportent n'importe comment ! je ne comprends pas pourquoi
-  // for (var i = R-1; i <= R+1; i++)  { // add mouvement adjacent
-  //   for (var j = C-2; j <= C+2; j++) {
-  //     if ((i !== R || j !== C) && in_board(i,j) && M[i][j] === -1) {
-  //       Tree[(i,j)] =  [(R,C), (i,j)];
-  //     }
-  //   }
-  // }
-  get_hope(R,C); // check for hopes
-  var newList;
-  while (liste.length > 0) { // recursif
-    newList = liste.shift() ;
-    if (newList != Start_Cell)
-      get_hope(newList[0], newList[1], Tree[newList]) ;
+  var i, j;
+  for (i =R-1; i <= R+1; i++)  {               // add mouvement adjaçant
+    for (j=C-2; j<= C+2; j++) {
+       if ((i!=R || j!=C) && in_board(i,j) && M[i][j]== -1)
+         Tree[(i,j)] =  [(R,C) ,(i,j)] ;
+    }
+  }
+  get_hope(R,C) ;
+  while (Liste) {
+    cell= Liste.shift() ;
+    if (cell != Start_Cell)
+      get_hope(cell[0], cell[1], Tree[cell]) ;
+  }
+}
+
+
+
+function get_hope(R, C, parent=0) {
+  var i, j, k;
+  var pivot_r , pivot_c;
+  var n, index;
+  if (!parent) parent = [(R,C)];
+  // chercher saut sur ligne dans 2 directions:
+  for (j of [(-2, 2)]) {                       // avancer de deux pas sur la ligne
+    pivot_c = C+j;
+    while (in_board(R, pivot_c) && Fld[R][pivot_c] == -1)           //avancer jusqu'a case occupée
+      pivot_c += j;
+    n=0
+    for (k=pivot_c+j; k<=2*pivot_c-C; k+=j) {
+      if (!in_board(R, k)) break ;
+      if (Fld[R][k] != -1) n+=1 ;         //si un autre pion sur le chemin
+    }
+    if (n==0) {                           // seulement s'il ya un seul pion sur le chemin servant de pivot
+       index = 2*pivot_c-C 
+       if ((R, index) in Tree) continue ;                    // éviter de tourner rond
+       if (in_board(R, index) && Fld[R][index]== -1) {     // si la case en symétrie est vide valide:  
+         Tree[(R,index)] = parent + [(R, index)];
+         Liste.push((R, index))
+       }
+    }
+  }
+  // chercher saut sur diagonal 4 directions :
+  for (i of [(-1, 1)]) {                        // avancer d'un seul pas sur le diagonal
+    for (j of [(-1, 1)]) {
+      pivot_r = R+i
+      pivot_c = C+j
+      while (in_board(pivot_r, pivot_c) && Fld[pivot_r][pivot_c] == -1) {            // avancer jusqu'a case occupée
+        pivot_r += i;
+        pivot_c += j;
+      }
+      n=0
+      for (k=1; k<= j * (pivot_c-C); k+=j) {
+        if (!in_board(pivot_r+i*k, pivot_c+j*k)) break;
+        if (Fld[pivot_r+i*k][pivot_c+j*k] != -1) n+=1;         // si un autre pion sur le chemin break
+      }  
+      if (n==0) {
+        index_r = 2*pivot_r-R;
+        index_c = 2*pivot_c-C;
+        if ((index_r, index_c) in Tree) continue ;                     // éviter de tourner rond
+        if (in_board(index_r, index_c) && Fld[index_r][index_c]== -1) {   // si la case en asymétrie est vide valide: 
+          self.Tree[(index_r,index_c)] = parent + [(index_r, index_c)];
+          self.liste.append((index_r, index_c));
+        }
+      }
+    }
   }
 }
 
@@ -170,9 +211,6 @@ function in_board(x,y) {
   return (-1<x<17 && -1<y<25);
 }
 
-function get_hope(R, C, parent) {
-
-}
 
 function make_move(mov_list) {
 
