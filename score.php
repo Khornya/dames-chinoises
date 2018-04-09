8ea2abf4e01117e47c595a622e26c49b4adf6ef4
valenrap <37075111+valenrap@users.noreply.github.com>
2018-04-09 20:49:42 +0200

    ajout bdd et redirection

--- score.php
+++ score.php
@@ -1,3 +1,17 @@
-à faire :
-récupérer name, score, date, adversaires via GET
-ajouter une entrée à la table score
+<?php
+if (isset($_GET['name']) AND isset($_GET['score']) AND ($_GET['score']<500) AND ($_GET['score']>0))
+{
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=dames_chinoises;charset=utf8', 'root', 'root');
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+	$name= $_GET['name'];
+	$score= (int) $_GET['score'];
+	$reponse = $bdd->query("INSERT INTO parties (ID, nom, score, dategame) VALUES (NULL,'$name','$score',now())");
+}
+echo "<meta http-equiv='refresh' content='0; url=home.php'>";
+?>
