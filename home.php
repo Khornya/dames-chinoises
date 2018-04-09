225733ff8064c0d37cc3d3d673f598b9192e38f0
Khornya <33462756+Khornya@users.noreply.github.com>
2018-04-09 16:51:37 +0200

    vérifications formulaire
    nom de 2 à 10 caractères, pas 2 joueurs avec le même nom
    tous caractères autorisés
    externalisation du JS

--- home.php
+++ home.php
@@ -31,7 +31,7 @@ session_start(); /* pour la gestion des sessions plus tard */
       <div id="test">
           <section id="game">
             <h1>Jouer une partie</h1>
-            <form action="game.php" target="_self" method="POST">
+            <form name="form" id="form" action="game.php" target="_self" method="POST">
               <select id="mode" name="nombre_joueurs" onclick="updateChoices()">
                 <option value="1">1 player vs CPU</option>
                 <option value="2">2 players</option>
@@ -41,22 +41,22 @@ session_start(); /* pour la gestion des sessions plus tard */
               </select>
               <br />
               <div>
-                <input type="text" name="player1" id="player1" placeholder="NAME PLAYER 1"/><span id="player1colors"></span>
+                <input type="text" name="player1" id="player1" placeholder="NAME PLAYER 1"/><span id="player1colors"></span><span class="tooltip"></span>
               </div>
               <div>
-                <input type="hidden" name="player2" id="player2" placeholder="NAME PLAYER 2"/><span id="player2colors"></span>
+                <input type="text" name="player2" id="player2" placeholder="NAME PLAYER 2"/><span id="player2colors"></span><span class="tooltip"></span>
               </div>
               <div>
-                <input type="hidden" name="player3" id="player3" placeholder="NAME PLAYER 3"/><span id="player3colors"></span>
+                <input type="text" name="player3" id="player3" placeholder="NAME PLAYER 3"/><span id="player3colors"></span><span class="tooltip"></span>
               </div>
               <div>
-                <input type="hidden" name="player4" id="player4" placeholder="NAME PLAYER 4"/><span id="player4colors"></span>
+                <input type="text" name="player4" id="player4" placeholder="NAME PLAYER 4"/><span id="player4colors"></span><span class="tooltip"></span>
               </div>
               <div>
-                <input type="hidden" name="player5" id="player5" placeholder="NAME PLAYER 5"/><span id="player5colors"></span>
+                <input type="text" name="player5" id="player5" placeholder="NAME PLAYER 5"/><span id="player5colors"></span><span class="tooltip"></span>
               </div>
               <div>
-                <input type="hidden" name="player6" id="player6" placeholder="NAME PLAYER 6"/><span id="player6colors"></span>
+                <input type="text" name="player6" id="player6" placeholder="NAME PLAYER 6"/><span id="player6colors"></span><span class="tooltip"></span>
               </div>
               <span id="color_choice">
                 Nombre de couleurs :
@@ -99,77 +99,6 @@ session_start(); /* pour la gestion des sessions plus tard */
         <a href="mailto:contact@hophophop.fr">Nous contacter</a>
       </footer>
     </div>
-  <script>
-    updateColors();
-
-    function seemore() {
-      var x = document.getElementById("seemore");
-      var y = document.getElementById("voirPlus");
-      if (x.style.display === "none") {  /* get computedStyle */
-          x.style.display = "block";
-          y.innerHTML = "Voir moins";
-      }
-      else {
-          x.style.display = "none";
-          y.innerHTML = "Voir plus";
-      }
-    }
-
-    function updateChoices() {
-      var colorChoices = {
-        1: { display: '', default: 1 },
-        2: { display: '', default: 1 },
-        3: { display: 'none', default: 2 },
-        4: { display: 'none', default: 1 },
-        6: { display: 'none', default: 1 }
-      };
-      for (var i = 1; i <= 6; i++) {
-        document.getElementById("player"+i).type = 'hidden';
-      }
-      var e = document.getElementById("mode");
-      var mode = e.options[e.selectedIndex].value;
-      document.getElementById("color_choice").style = "display:" + colorChoices[mode]['display'];
-      for (var i=1; i<=3; i++) {
-        document.getElementById(i).checked = (i === colorChoices[mode].default) ? true : false;
-      }
-      for (var i = 1; i <= mode; i++) {
-        document.getElementById("player"+i).type = 'text';
-      }
-      updateColors();
-    }
-
-    function updateColors() {
-      var e = document.getElementById("mode");
-      var mode = parseInt(e.options[e.selectedIndex].value);
-      for (var n_color = 1; n_color <= 3; n_color++) {
-        if (document.getElementById(n_color).checked) break;
-      }
-      var colors = { // attribution des couleurs à chaque joueur
-        1: {
-          1: [[1],[2]],
-          2: [[1,3],[2,4]],
-          3: [[1,3,5],[2,4,6]]
-        },
-        2: {
-          1: [[1],[2]],
-          2: [[1,3],[2,4]],
-          3: [[1,3,5],[2,4,6]]
-        },
-        3: { 2: [[1,3],[4,5],[2,6]] },
-        4: { 1: [[1],[2],[3],[4]] },
-        6: { 1: [[1],[3],[6],[2],[4],[5]] }
-      }[mode][n_color];
-      for (var n=1; n<=mode; n++) {
-        var code = '';
-        for (var i=0, max = colors[n-1].length; i < max; i++) {
-          code += "<img class='imagetag' src='images/pion" + colors[n-1][i] + ".png'/>";
-        }
-        document.getElementById('player'+n+'colors').innerHTML = code;
-      }
-      for (var n=mode+1; n<=6; n++) {
-        document.getElementById('player'+n+'colors').innerHTML = '';
-      }
-    }
-  </script>
+  <script src="home.js"></script>
   </body>
 </html>
