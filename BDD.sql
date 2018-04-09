c8b4c2f896ed5a6d165724780645c63f6b954c6a
valenrap <37075111+valenrap@users.noreply.github.com>
2018-04-09 18:45:51 +0200

    nouvelle table "parties"

--- BDD.sql
+++ BDD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Mar 03 Avril 2018 à 10:30
+-- Généré le :  Lun 09 Avril 2018 à 18:44
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.0.15
 
@@ -17,36 +17,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `scores`
+-- Structure de la table `parties`
 --
 
-CREATE TABLE `scores` (
+CREATE TABLE `parties` (
   `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `score` int(11) NOT NULL,
-  `adversaire` varchar(50) NOT NULL,
-  `date` date NOT NULL
+  `adversaire1` varchar(50) NOT NULL,
+  `adversaire2` varchar(50) DEFAULT NULL,
+  `adversaire3` varchar(50) DEFAULT NULL,
+  `adversaire4` varchar(50) DEFAULT NULL,
+  `adversaire5` varchar(50) DEFAULT NULL,
+  `dategame` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contenu de la table `scores`
+-- Contenu de la table `parties`
 --
 
-INSERT INTO `scores` (`id`, `nom`, `score`, `adversaire`, `date`) VALUES
-(1, 'JOUEUR 1', 15, 'JOUEUR 2', '2018-04-03'),
-(2, 'JOUEUR 3', 14, 'JOUEUR 4', '2018-04-03'),
-(3, 'JOUEUR 5', 13, 'JOUEUR 7', '2018-04-03'),
-(4, 'JOUEUR 3', 11, 'JOUEUR 4', '2018-04-03'),
-(5, 'JOUEUR 9', 18, 'JOUEUR 4', '2018-04-03');
+INSERT INTO `parties` (`id`, `nom`, `score`, `adversaire1`, `adversaire2`, `adversaire3`, `adversaire4`, `adversaire5`, `dategame`) VALUES
+(48, 'A', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(49, 'B', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(50, 'PLAYER 2', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(51, 'C', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(52, 'D', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(53, 'E', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(54, 'F', 0, '', NULL, NULL, NULL, NULL, '2018-04-08'),
+(55, 'G', 0, '', NULL, NULL, NULL, NULL, '2018-04-08');
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `scores`
+-- Index pour la table `parties`
 --
-ALTER TABLE `scores`
+ALTER TABLE `parties`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -54,7 +61,7 @@ ALTER TABLE `scores`
 --
 
 --
--- AUTO_INCREMENT pour la table `scores`
+-- AUTO_INCREMENT pour la table `parties`
 --
-ALTER TABLE `scores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `parties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
